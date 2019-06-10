6165cd080bef270c3cea6792ad42fffc37ee700c
xu <sudoxu@163.com>
2019-06-10 18:27:23 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastClick from 'fastClick'
-import './assets/styles/normalize.css'
-import './assets/styles/border.css'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 // 解决移动端按钮点击事件300毫秒的延迟
