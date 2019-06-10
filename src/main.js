135ba770becc4034fc3830d8bad38e0c23f8d4c1
xu <sudoxu@163.com>
2019-06-10 09:14:28 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+// 入口代码
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastClick'
+import './assets/styles/normalize.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+// 解决移动端按钮点击事件300毫秒的延迟
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -13,3 +18,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+// 路由就是根据网址的不同,返回不同的内容给用户
