7cc62c8e7559c77291dc9ca6ed0c1f9733241beb
xu <sudoxu@163.com>
2019-06-11 17:13:19 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -34,10 +34,10 @@ export default {
         },
         swiperSlides: [{
           id:'0001',
-          imageUrl:'http://i0.hdslb.com/bfs/article/a8c6df0dd7455fdb133575965d0719ee17aab281.png'
+          imageUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg'
         },{
           id:'0002',
-          imageUrl:'http://i0.hdslb.com/bfs/article/802b949231440fef8ed8af718750e4ff25189b24.jpg'
+          imageUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/3a3066c073a492c1a881607181ac84ab.jpg_750x200_ca7fff4c.jpg'
         }]
       }
     }
@@ -51,7 +51,7 @@ export default {
   overflow: hidden
   width: 100%
   height: 0
-  padding-bottom: 31.25%
+  padding-bottom: 26.66%
   background:#eee
   .swiper-img
     width: 100%
